c1bec69376a50b71c6247c3057ac096d575b1b66
ryansperzel <ryansperzel@gmail.com>
2017-09-28 11:25:20 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,48 @@
 class Song < ActiveRecord::Base
+
+  attr_reader :song_genre_id
+
+
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_by(name: name)
+  end
+
+  def genre_name
+    self.genre.name
+  end
+
+  def note_contents=(contents)
+    contents = contents.reject do |c|
+      c.empty?
+    end
+     contents.each do |content|
+       note = Note.find_or_create_by(content: content)
+       self.notes << note
+     end
+   end
+
+   def note_contents
+     self.notes.map do |note|
+       note.content
+     end
+   end
+
+   def artist_name
+     if artist.nil?
+       nil
+     else
+       self.artist.name
+     end
+   end
+
+
+end
